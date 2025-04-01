1521ee11cd717d7d6a712d83c214338a9d520f0a
Your Name <your-email@example.com>
2025-04-01 15:26:14 +0100

    UserMgmt

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://ashutoshgau3-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://ashutoshgau3-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -61,7 +61,9 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ os.path.join(BASE_DIR, 'frontend/static')],
+        'DIRS': [ os.path.join(BASE_DIR, 'frontend/static'),
+                 os.path.join(BASE_DIR, 'frontend/build'),
+                  os.path.join(BASE_DIR, 'frontend/build/static') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,7 +136,6 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-     os.path.join(BASE_DIR,'frontend/static/')
-]
-
+STATICFILES_DIRS =[ os.path.join(BASE_DIR, 'frontend/static'),
+                    os.path.join(BASE_DIR, 'frontend/build'),
+                    os.path.join(BASE_DIR, 'frontend/build/static') ]
