ee248daba83d0afdcb61ae23a60b065e1c8d8132
Stiveve <ashutoshgautam3006@gmail.com>
2025-04-02 08:25:11 -0400

    changes made from theia lab environment

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,6 +6,10 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     # # path for registration
     path(route='register', view=views.registration, name='register'),
     # path for cars
@@ -19,5 +23,6 @@ urlpatterns = [
     # path for dealer reviews view
 
     # path for add a review view
+    path(route='add_review', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
