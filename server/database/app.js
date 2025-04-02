7bb811786ecec220077fb5d1c194d3745364e9c9
Stiveve <ashutoshgautam3006@gmail.com>
2025-04-02 09:45:37 -0400

    lint changes

--- server/database/app.js
+++ server/database/app.js
@@ -6,7 +6,7 @@ const  cors = require('cors');
 const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -21,7 +21,7 @@ const Dealerships = require('./dealership');
 
 try {
   Reviews.deleteMany({}).then(()=>{
-    Reviews.insertMany(reviews_data['reviews']);
+    Reviews.insertMany(reviews_data.reviews);
   });
   Dealerships.deleteMany({}).then(()=>{
     Dealerships.insertMany(dealerships_data.dealerships);
@@ -34,7 +34,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -94,7 +94,7 @@ app.get('/fetchDealer/:id', async (req, res) => {
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
   const documents = await Reviews.find().sort( { id: -1 } );
-  let new_id = documents[0]['id']+1;
+  let new_id = documents[0].id+1;
 
   const review = new Reviews({
 	id: new_id,
